a3ef5ee83f1bed4658a2d6105a1c62b4b0fae0ec
Leonel Garcia <l.garcia@estudiantes.matcom.uh.cu>
2022-02-14 17:36:24 -0500

    Utils functions to parse responses

---
+++ tfprotocol_client/misc/parse_utils.py
@@ -0,0 +1,20 @@
+import re
+from typing import Tuple
+
+
+def separate_status_name(string: str):
+    res = re.split(r'\s+', string.strip(), maxsplit=1)
+    # res_f = [r.strip() for r in res if r is not None and r.strip() != '']
+    if len(res) > 1:
+        return res[0], ''.join(res[1:])
+    else:
+        return res[0], ''
+
+
+def separate_status_codenumber(string: str) -> Tuple[str, str]:
+    res = re.split(r'([-+]?\d+\.\d+)|([-+]?\d+)', string.strip(), maxsplit=1)
+    res_f = [r.strip() for r in res if r is not None and r.strip() != '']
+    if len(res_f) > 1 and res_f[0].isdecimal():
+        return res_f[0], ''.join(res_f[1:])
+    else:
+        return '', ''.join(res_f)
