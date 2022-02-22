ae991535e49b9a17facf01f2125129e3669e9211
Leonel Garcia <l.garcia@estudiantes.matcom.uh.cu>
2022-02-21 22:09:54 -0500

    Handy classes for putcan and getcan commands

---
+++ tfprotocol_client/models/putget_commands.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class PutGetCommandEnum(Enum):
+    """Commands used to put and get commands optimization."""
+
+    HPFEND = 0
+    HPFSTOP = -1
+    HPFCANCEL = -2
+    HPFCONT = -3
+    HPFFIN = -127
---
+++ tfprotocol_client/models/transfer_state.py
@@ -0,0 +1,7 @@
+class TransferStatus:
+    """Transfer status"""
+
+    def __init__(self) -> None:
+        self.dummy = None
+        self.command = None
+        self.dummy_state = None
