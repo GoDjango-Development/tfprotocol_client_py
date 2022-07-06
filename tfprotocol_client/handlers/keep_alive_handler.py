16d146cf4c1dfb87344f8cbe1a92187fe260c73f
Leonel Garcia <lagcleaner@gmail.com>
2022-07-06 00:25:34 -0400

    Remove handlers classes

--- tfprotocol_client/handlers/keep_alive_handler.py
+++
@@ -1,9 +0,0 @@
-# coded by lagcleaner
-# email: lagcleaner@gmail.com
-
-
-class KeepAliveHandler:
-    '''The callback system where server connection close is notified.'''
-
-    def connection_closed(self):
-        pass
