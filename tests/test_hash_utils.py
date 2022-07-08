30c459403c51ec0064488330a92e918e5212ce56
Leonel Garcia <lagcleaner@gmail.com>
2022-07-08 13:09:42 -0400

    Add tests for status info and hash library

---
+++ tests/test_hash_utils.py
@@ -0,0 +1,10 @@
+from tfprotocol_client.security.hash_utils import sha256_for
+
+
+def test_hash():
+    """Test the hash utils"""
+    assert (
+        sha256_for('esto es probando')
+        == b'\xbdB\xa31\x11\x94\x1bP\xbf\xc8r\xa4\xb7\xf7\x1f\xbee\xca\xf35\xcaj\xbd\x0c\xe0e\xf6\x83X\x87\xb3\x0f'
+    )
+    assert sha256_for('esto es probando') == sha256_for(b'esto es probando')
---
+++ tests/test_status_info.py
@@ -0,0 +1,51 @@
+from tfprotocol_client.models.status_info import StatusInfo
+from tfprotocol_client.models.status_server_code import StatusServerCode
+
+
+def test_status_info_build():
+    """Test for status_info builder function"""
+    # test OK
+    status = StatusInfo.build_status(2, b'OK')
+    assert status.status == StatusServerCode.OK
+    assert status.code == 0
+    assert status.payload == b''
+    assert status.message == ''
+    # test FAILED
+    status = StatusInfo.build_status(30, b'FAILED 5: a lot of info before')
+    assert status.status == StatusServerCode.FAILED
+    assert status.message == 'a lot of info before'
+    assert status.code == 5
+    assert status.payload == b'a lot of info before'
+
+    # test RAW MESSAGE
+    status = StatusInfo.build_status(38, b'A lot of info whitout header or status')
+    assert status.status == StatusServerCode.UNKNOWN
+    assert status.message == 'A lot of info whitout header or status'
+    assert status.code == 38
+    assert status.payload == b'A lot of info whitout header or status'
+
+
+def test_status_info_parse():
+    """Test for status_info parse function"""
+
+    # test OK
+    status = StatusInfo.parse('OK')
+    assert status.status == StatusServerCode.OK
+    assert status.code == 0
+    assert status.message == ''
+    assert status.payload == b''
+    # test FAILED
+    status = StatusInfo.parse('FAILED 5: a lot of info before')
+    assert status.status == StatusServerCode.FAILED
+    assert status.message == 'a lot of info before'
+    assert status.code == 5
+    assert status.payload == b''
+
+    # test RAW MESSAGE
+    status = StatusInfo.parse(
+        'A lot of info whitout header or status', payload=b'A payload'
+    )
+    assert status.status == StatusServerCode.UNKNOWN
+    assert status.message == 'A lot of info whitout header or status'
+    assert status.code == 38
+    assert status.payload == b'A payload'
