817508a94cd7c0b2a920dfca12312057758522c4
Leonel Garcia <lagcleaner@gmail.com>
2022-07-08 13:19:59 -0400

    Add test template for tfprotocol super

---
+++ tests/tfprotocol_super/test_tfprotocol_super.py
@@ -0,0 +1,6 @@
+from tfprotocol_client.tfprotocol_super import TfProtocolSuper
+
+def test_tfprotocol_super_connection():
+    """Test for tfprotocol_super"""
+    # tfproto = TfProtocolSuper()
+    assert True
