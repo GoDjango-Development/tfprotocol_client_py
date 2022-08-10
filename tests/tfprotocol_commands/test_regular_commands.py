ca1f544d4a544201fb16899c6f71f8dd79eda995
Leonel Garcia <lagcleaner@gmail.com>
2022-08-10 02:04:27 -0400

    Add tests for FREESP, SHA256, PROCKEY and END commands

--- tests/tfprotocol_commands/test_regular_commands.py
+++ tests/tfprotocol_commands/test_regular_commands.py
@@ -95,7 +95,48 @@ def test_udate_command(tfprotocol_instance: TfProtocol):
     assert resps[0].status == StatusServerCode.OK, resps[0]
     assert resps[0].message is not None and resps[0].message != '', resps[0]
 
+
+# @pytest.mark.run(order=38)
+# def test_nigma_command(tfprotocol_instance: TfProtocol):
+#     """Test for nigma command."""
+#     # NOTE: This test is not reliable, because the server is not working properly.
+#     tfproto = tfprotocol_instance
+#     resps: List[StatusInfo] = []
+#     # NIGMA command - OK
+#     old_key = tfproto.client.session_key
+#     tfproto.nigma_command(KEY_LEN_INTERVAL[0], response_handler=resps.append)
+#     assert old_key != tfproto.client.session_key, resps[0]
+
+#     # Exception case
+#     with pytest.raises(TfException):
+#         tfproto.nigma_command(9, response_handler=resps.append)
+
+
 @pytest.mark.run(order=39)
+def test_freesp_command(tfprotocol_instance: TfProtocol):
+    """Test for freesp command."""
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    tfproto.freesp_command(response_handler=resps.append)
+    # FREESP command
+    assert len(resps) == 1, resps
+    assert resps[0].status == StatusServerCode.OK, resps[0]
+    assert resps[0].message is not None and resps[0].message != '', resps[0]
+
+
+@pytest.mark.run(order=39)
+def test_prockey_command(tfprotocol_instance: TfProtocol):
+    """Test for prockey command."""
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    tfproto.prockey_command(response_handler=resps.append)
+    # PROCKEY command
+    assert len(resps) == 1, resps
+    assert resps[0].status == StatusServerCode.OK, resps[0]
+    assert resps[0].message is not None and len(resps[0].message) == 16, resps[0]
+
+
+@pytest.mark.run(order=40)
 def test_fpub_command(tfprotocol_instance: TfProtocol):
     """Test for fpub command."""
     tfproto = tfprotocol_instance
@@ -113,3 +154,30 @@ def test_fpub_command(tfprotocol_instance: TfProtocol):
     # assert resps[-1][0].last_modification > resps[-1][0].last_access, resps[-1]
     #
     tfproto.del_command('/py_test/file.txt')
+
+
+@pytest.mark.run(order=41)
+def test_sha256_command(tfprotocol_instance: TfProtocol):
+    """Test for sha256 command."""
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    tfproto.touch_command('/py_test/file.txt')
+    # SHA256 command
+    tfproto.sha256_command('/py_test/file.txt', response_handler=resps.append)
+    assert resps[-1].status == StatusServerCode.OK, resps[-1]
+    assert resps[-1].message == "0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", resps[-1]
+    #
+    tfproto.del_command('/py_test/file.txt')
+
+@pytest.mark.run(order=42)
+def test_end_command(tfprotocol_instance: TfProtocol):
+    """Test for end command."""
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    # END command
+    tfproto.end_command()
+    with pytest.raises(TfException):
+        tfproto.echo_command('Hello', response_handler=resps.append)
+    #
+    tfproto.connect()
+    tfproto.connect()
