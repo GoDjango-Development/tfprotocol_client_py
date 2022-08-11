41258059e905d4bf987c81cfd9552d7e78a11245
Leonel Garcia <lagcleaner@gmail.com>
2022-08-11 10:46:33 -0400

    Add tests for SNDFILE and RCVFILE commands

--- tests/tfprotocol_commands/test_file_transference_commands.py
+++ tests/tfprotocol_commands/test_file_transference_commands.py
@@ -44,3 +44,44 @@ def test_sup_sdown_commands(tfprotocol_instance: TfProtocol):
     #
     tfproto.del_command('py_test/test_supdown.txt')
 
+
+def __rcvfile_handler(
+    status: StatusInfo,
+    sink: io.BytesIO = None,
+    resps: List[StatusInfo] = None,
+):
+    if sink is not None and sink.writable() and status.payload:
+        sink.write(status.payload)
+    resps.append(status)
+
+
+@pytest.mark.run(order=50)
+def test_snd_rcv_commands(tfprotocol_instance: TfProtocol):
+    """Test for sndfile and rcvfile commands."""
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    #
+    # SENDFILE
+    tfproto.sndfile_command(
+        True,
+        'py_test/test_sendrcv.txt',
+        io.BytesIO(b'Some random text'),
+        lambda _, __, status, ___: resps.append(status),
+    )
+    assert all((r.status == StatusServerCode.CONT) for r in resps[:-1])
+    assert resps[-1].status == StatusServerCode.OK
+    resps.clear()
+    #
+    # RECVFILE
+    sink = io.BytesIO()
+    tfproto.rcvfile_command(
+        True,
+        'py_test/test_sendrcv.txt',
+        sink,
+        lambda _, __, status, sink: __rcvfile_handler(status, sink, resps),
+    )
+    assert sink.getvalue() == b'Some random text'
+    assert all((r.status == StatusServerCode.CONT) for r in resps[:-1])
+    assert resps[-1].status == StatusServerCode.OK
+    #
+
