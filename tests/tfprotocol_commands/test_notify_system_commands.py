6927a62b5f4958b5774038c2fe179823768a282c
Leonel Garcia <lagcleaner@gmail.com>
2022-08-16 12:53:50 -0400

    Fix warnings in test notify system file

--- tests/tfprotocol_commands/test_notify_system_commands.py
+++ tests/tfprotocol_commands/test_notify_system_commands.py
@@ -3,15 +3,12 @@
 
 # pylint: disable=redefined-outer-name
 
-import io
 from time import sleep
 from typing import Callable, List
 
 import pytest
-from tfprotocol_client.models.putget_commands import PutGetCommandEnum
 from tfprotocol_client.models.status_info import StatusInfo
 from tfprotocol_client.models.status_server_code import StatusServerCode
-from tfprotocol_client.models.transfer_state import TransferStatus
 from tfprotocol_client.tfprotocol import TfProtocol
 
 # pylint: disable=unused-import
@@ -20,8 +17,8 @@ from .tfprotocol import tfprotocol_instance
 
 def __ntfy_handler(
     status: StatusInfo,
-    send_ok: Callable,
-    send_del: Callable,
+    _: Callable,
+    __: Callable,
     resps: List[StatusInfo] = None,
     callback: Callable = lambda: None,
 ) -> None:
