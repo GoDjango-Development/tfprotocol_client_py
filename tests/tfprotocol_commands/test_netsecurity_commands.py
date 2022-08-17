6b531815bcf80b3afa146dc3d405dd75132d1944
Leonel Garcia <lagcleaner@gmail.com>
2022-08-17 12:49:26 -0400

    Add tests for NETLOCK, NETLOCKTRY and NETUNLOCK commands

--- tests/tfprotocol_commands/test_netsecurity_commands.py
+++ tests/tfprotocol_commands/test_netsecurity_commands.py
@@ -3,16 +3,75 @@
 
 # pylint: disable=redefined-outer-name
 
-import io
-from time import sleep
-from typing import Callable, List
+from typing import List, Optional
 
 import pytest
-from tfprotocol_client.models.putget_commands import PutGetCommandEnum
+from tfprotocol_client.misc.constants import SECFS_ALL_PERMISSIONS
 from tfprotocol_client.models.status_info import StatusInfo
 from tfprotocol_client.models.status_server_code import StatusServerCode
-from tfprotocol_client.models.transfer_state import TransferStatus
 from tfprotocol_client.tfprotocol import TfProtocol
 
 # pylint: disable=unused-import
 from .tfprotocol import tfprotocol_instance
+
+__LOCK_ID: Optional[str] = None
+
+
+@pytest.mark.run(order=70)
+def test_netlock_command(tfprotocol_instance: TfProtocol):
+    """Test for netlock command."""
+    # pylint: disable=global-statement
+    global __LOCK_ID
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    tfproto.touch_command('/py_test/test.lock')
+    # NETLOCK command
+    tfproto.netlock_command(
+        5,
+        '/py_test/test.lock',
+        response_handler=resps.append,
+    )
+    assert resps[0].status == StatusServerCode.OK, resps[0]
+    assert resps[0].message is not None, resps[0]
+    #
+    __LOCK_ID = resps[0].message
+    #
+    # tfproto.del_command('/py_test/test.lock')
+
+
+@pytest.mark.run(order=71)
+def test_netlocktry_command(tfprotocol_instance: TfProtocol):
+    """Test for netlocktry command."""
+    # pylint: disable=global-statement
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    tfproto.touch_command('/py_test/testtry.lock')
+    # NETLOCKTRY command
+    tfproto.netlocktry_command(
+        5,
+        '/py_test/testtry.lock',
+        response_handler=resps.append,
+    )
+    assert resps[0].status == StatusServerCode.OK, resps[0]
+    assert resps[0].message is not None, resps[0]
+    #
+    #
+    # tfproto.del_command('/py_test/testtry.lock')
+
+
+@pytest.mark.run(order=72)
+def test_netunlock_command(tfprotocol_instance: TfProtocol):
+    """Test for netunlock command."""
+    # pylint: disable=global-statement
+    tfproto = tfprotocol_instance
+    resps: List[StatusInfo] = []
+    # NETUNLOCK command
+    tfproto.netunlock_command(
+        __LOCK_ID,
+        response_handler=resps.append,
+    )
+    assert resps[0].status == StatusServerCode.OK, resps[0]
+    assert resps[0].message is not None, resps[0]
+    #
+    tfproto.del_command('/py_test/test.lock')
+
