5bbca709612ae0facfd7457b7906c00d069a2676
Leonel Garcia <lagcleaner@gmail.com>
2022-08-25 01:11:29 -0400

    Add test for EXEC from SQLite subsystem command

--- tests/xs_sqlite/test_xs_sqlite_commands.py
+++ tests/xs_sqlite/test_xs_sqlite_commands.py
@@ -3,7 +3,7 @@
 
 # pylint: disable=redefined-outer-name
 
-from datetime import datetime
+
 from typing import List, Tuple
 
 import pytest
@@ -42,6 +42,58 @@ def test_xssqlite_open_close_commands(xssqlite_instance: XSSQLite):
     tfproto.close_command(db_id, response_handler=resps.append)
     assert resps[-1].status == StatusServerCode.OK, resps[-1]
     assert resps[-1].message not in (None, ''), resps[-1]
-    
-    assert resps[0].status == StatusServerCode.OK, resps[0]
-    assert resps[0].message not in (None, ''), resps[0]
+
+
+@pytest.mark.depends(on=['test_xssqlite_command', 'test_xssqlite_open_close_commands'])
+def test_xssqlite_exec_command(xssqlite_instance: XSSQLite):
+    """Test for exec command."""
+    tfproto = xssqlite_instance
+    resps: List[StatusInfo] = []
+    tfproto.open_command('py_test.db', response_handler=resps.append)
+    db_id = resps[-1].message.split()[-1]
+    # EXEC command (With no data)
+    tfproto.exec_command(
+        db_id,
+        '''
+        DROP TABLE IF EXISTS COMPANY;
+        CREATE TABLE COMPANY(
+            ID INT PRIMARY KEY     NOT NULL,
+            NAME           TEXT    NOT NULL,
+            AGE            INT     NOT NULL,
+            ADDRESS        CHAR(50),
+            SALARY         REAL
+        );
+        ''',
+        response_handler=resps.append,
+    )
+    assert resps[-1].status == StatusServerCode.OK, resps[-1]
+    assert resps[-1].message not in (None, ''), resps[-1]
+    tfproto.exec_command(
+        db_id,
+        '''
+        INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+        VALUES (1, 'Paul', 32, 'California', 20000.00 );
+
+        INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+        VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
+
+        INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+        VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
+        ''',
+    )
+    rows : List[list] = []
+    # EXEC command (With data)
+    tfproto.exec_command(
+        db_id,
+        '''SELECT * FROM COMPANY;''',
+        response_handler=resps.append,
+        rows_handler=rows.append,
+    )
+    assert resps[-1].status == StatusServerCode.OK, resps[-1]
+    assert resps[-1].message not in (None, ''), resps[-1]
+    assert len(rows) == 4, rows
+    assert rows[0][0] == b'ID', rows[0]
+    assert rows[1][0] == b'1', rows[1]
+    #
+    tfproto.exec_command(db_id, '''DROP TABLE COMPANY;''')
+    tfproto.close_command(db_id)
